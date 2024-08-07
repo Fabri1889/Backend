99c5472a7775075b22f0945d588f687ab36a6541
Fabri1889 <131361629+Fabri1889@users.noreply.github.com>
2024-08-07 01:28:17 -0300

    Update index.js
    se comento el codigo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import app from "./app.js";
 import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
 
+// Función principal para arrancar la aplicación
 async function main() {
   try {
-    await connectDB();
-    app.listen(PORT);
+    await connectDB(); // Conecta a la base de datos
+    app.listen(PORT); // Inicia el servidor
     console.log(`Listening on port http://localhost:${PORT}`);
     console.log(`Environment: ${process.env.NODE_ENV}`)
   } catch (error) {
